3ea6692550cfcf5a74675d465276c0f9729740b1
dcaustin33 <dcaustin33@gmail.com>
2024-07-24 07:36:48 -0400

    download model

--- detection_setup.sh
+++ detection_setup.sh
@@ -5,5 +5,5 @@ pip install timm==0.4.9 einops;
 pip install -r requirements.txt;
 if [ ! -d "vitpose_base_coco_aic_mpii.pth" ]; then
     echo "Downloading the model checkpoint...";
-    wget "https://6mafda.dm.files.1drv.com/y4mmIJNdva0GSE02wT52EQuFmSznit6ImM0t3esSsl3NMZt0_uTf93IohQSyak8JliqfSwuolqB4jChvQaA8PkjudYpX9cFoMJyRwB0A0G_asIHEh51TUIw_BYPyN9fY3qo0h82BjvdI1UC1u9jObNrWIotxzdwdbVE63h6Xnv5_6TCyOb6yl9zdJ_dDaZZ4i2DmnOQtSylfgmnGCTA89qz3g";
+    wget "https://6mahda.dm.files.1drv.com/y4m6-jfGNU1CYSpmF4F-QbLryegdV2kD-OCZunzCsp2JhFVypkB8BShyPaILlUGv7uBHUNzmsl4v773c0Jt1pdMrzBHhCJ8n46DG2lovYHxfX-xkYyQH3XqVrtFYt84g8N2bHGxygMTEzTxM05S4XJrvzzh4-9VpxDsgn2V1FJjEiRhiu18hIdo0iC0hKGLwdy5wA2O5A6pu9yjhXynAtQ2pw" -O vitpose_base_coco_aic_mpii.pth;
 fi
